039dfa944739c2db4f5909a1a7eecf1694d70185
Gromf1 <levchukgeorgy1@gmail.com>
2026-07-14 15:36:50 +0500

    Первичный скелет веб-сервиса1

---
+++ src/test/java/ru/oms66/med_report_service/MedReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oms66.med_report_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
